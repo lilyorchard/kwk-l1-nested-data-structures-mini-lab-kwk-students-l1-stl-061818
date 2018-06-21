fb5df53ed65b8916df8c9bb8a7c414f770a56796
lilyorchard <lily@orchardfamily.com>
2018-06-21 16:15:43 +0000

    automatically backed up by learn

--- get_input.rb
+++ get_input.rb
@@ -1,3 +1,3 @@
 puts "please input you birthmonth"
 birthmonth = gets.chomp
-puts "Oh, that's great! Birthdays in #{birthmonth.capitalize}!"
+puts "Oh, that's great! Birthdays in #{birthmonth.capitalize} are the best!"
